a56d1e5fc489cd0c87336cb50ee33d40059816ea
Heet Doshi <doshiheet2872003@gmail.com>
2024-03-15 12:23:17 +0530

    Ngrx

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,40 +1,32 @@
 import {
-    ActionReducer,
-    ActionReducerMap,
-    createFeatureSelector, createReducer,
-    createSelector,
-    MetaReducer, on
-} from '@ngrx/store';
-import {User} from '../model/user.model';
-import {AuthActions} from '../action-types';
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on,
+  State,
+} from "@ngrx/store";
+import { User } from "../model/user.model";
+import { AuthActions } from "../action-types";
 
-
-
-export interface AuthState {
-    user: User
+export interface AuthState{
+  user:User
 }
+export const initialAuthState : AuthState ={
+  user:undefined
+}
+// export const reducers : ActionReducerMap<AuthState>={
 
-export const initialAuthState: AuthState = {
-    user: undefined
-};
+// };
+export const  authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state , action) =>{
 
-export const authReducer = createReducer(
-
-    initialAuthState,
-
-    on(AuthActions.login, (state, action) => {
-        return {
-            user: action.user
-        }
-    }),
-
-    on(AuthActions.logout, (state, action) => {
-        return {
-            user: undefined
-        }
-    })
-
-
-
-);
+    return{
+      user:action.user
+    }
+  }),
+)
 
